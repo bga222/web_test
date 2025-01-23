579e6247f5bd93362becc6256bb47c8378950fdb
bga222 <sm23.bgarcia@iessantamariadeivissa.com>
2025-01-23 15:58:57 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,6 +13,6 @@ window.addEventListener('scroll', function() {
 
 // Cuando el usuario haga clic en el botón, subir al principio del documento
 function topFunction() {
-  document.body.scrollTop = 568940; // Para Safari
+  // document.body.scrollTop = 568940; // Para Safari
   document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 }
